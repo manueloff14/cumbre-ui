4a0a96bf6554b805ce351169f3c624cf96405caa
manueloff14 <manueloffcabrera@gmail.com>
2024-09-07 15:07:18 +0000

    Ruta dinamica, beta 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-// import node from '@astrojs/node';
-// import vercel from '@astrojs/vercel';
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'server'
-  /*adapter: vercel({
-    mode: 'standalone', // Especifica 'standalone' o 'middleware' según tus necesidades
-  }), */
+  output: 'server',
+  adapter: vercel()
 });
