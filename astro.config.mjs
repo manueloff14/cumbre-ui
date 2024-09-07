e39a228eb1c73f81adffff8ae1d2f6af883899ce
manueloff14 <manueloffcabrera@gmail.com>
2024-09-06 20:09:34 -0500

    Cambios 5

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import node from '@astrojs/node';
-import vercel from '@astrojs/vercel/server';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   output: 'server',
-  adapter: vercel()
+  adapter: vercel({
+    mode: 'standalone', // Especifica 'standalone' o 'middleware' según tus necesidades
+  }),
 });
