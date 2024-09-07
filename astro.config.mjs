737a6cb9ca945d61c280525cd6d7b5fb1077ecfd
manueloff14 <manueloffcabrera@gmail.com>
2024-09-06 20:13:32 -0500

    Cambios 8

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import node from '@astrojs/node';
-import vercel from '@astrojs/vercel';
+// import node from '@astrojs/node';
+// import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
