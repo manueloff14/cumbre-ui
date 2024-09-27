d2849f3c452edd08bce58efaeaa57adc5a99104b
manueloff14 <manueloffcabrera@gmail.com>
2024-09-27 11:44:59 -0700

    Adapter Vercel()

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import node from '@astrojs/node';
-/* import vercel from '@astrojs/vercel/serverless'; */
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   output: 'server',
-  /* adapter: vercel(), */
-  adapter: node({
-    mode: 'standalone' // O también puede ser 'middleware' dependiendo de tus necesidades
-  })
+  adapter: vercel()
 });
