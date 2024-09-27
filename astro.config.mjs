ee3e63aa44c52ab7a61aea987a4e7827c34b173f
manueloff14 <manueloffcabrera@gmail.com>
2024-09-27 10:34:45 -0700

    Cambios a Github Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://cumbre-ui.github.io',
   integrations: [tailwind()],
   output: 'server',
   adapter: vercel()
